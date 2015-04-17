d02d42df17fc52d9bdfcdffe8ea2d09058d812f9
BBuchala <bbuchala93@gmail.com>
2015-04-17 23:04:07 +0200

    Rejestracja v 1.1
    Teraz z możliwością rejestracji jako prowadzący, ale akceptować go musi
    admin.

--- ProjektBD/Forms/RegisterForm.Designer.cs
+++ ProjektBD/Forms/RegisterForm.Designer.cs
@@ -45,65 +45,75 @@
             this.label8 = new System.Windows.Forms.Label();
             this.index = new System.Windows.Forms.MaskedTextBox();
             this.birthDate = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // login
             // 
+            this.login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.login.Location = new System.Drawing.Point(11, 79);
             this.login.MaxLength = 50;
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(311, 20);
+            this.login.Size = new System.Drawing.Size(311, 21);
             this.login.TabIndex = 0;
             // 
             // password1
             // 
+            this.password1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.password1.Location = new System.Drawing.Point(11, 129);
             this.password1.MaxLength = 50;
             this.password1.Name = "password1";
             this.password1.PasswordChar = '*';
-            this.password1.Size = new System.Drawing.Size(311, 20);
+            this.password1.Size = new System.Drawing.Size(311, 21);
             this.password1.TabIndex = 1;
             // 
             // password2
             // 
+            this.password2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.password2.Location = new System.Drawing.Point(11, 180);
             this.password2.MaxLength = 50;
             this.password2.Name = "password2";
             this.password2.PasswordChar = '*';
-            this.password2.Size = new System.Drawing.Size(311, 20);
+            this.password2.Size = new System.Drawing.Size(311, 21);
             this.password2.TabIndex = 2;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(196, 289);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(167, 331);
             this.dateTimePicker1.MaxDate = new System.DateTime(2005, 12, 31, 0, 0, 0, 0);
             this.dateTimePicker1.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(126, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(154, 21);
             this.dateTimePicker1.TabIndex = 5;
             this.dateTimePicker1.Value = new System.DateTime(1990, 1, 1, 0, 0, 0, 0);
             // 
             // email
             // 
+            this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.email.Location = new System.Drawing.Point(11, 230);
             this.email.MaxLength = 50;
             this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(311, 20);
+            this.email.Size = new System.Drawing.Size(311, 21);
             this.email.TabIndex = 3;
             // 
             // address
             // 
-            this.address.Location = new System.Drawing.Point(11, 344);
+            this.address.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.address.Location = new System.Drawing.Point(10, 281);
             this.address.MaxLength = 100;
             this.address.Name = "address";
-            this.address.Size = new System.Drawing.Size(310, 20);
+            this.address.Size = new System.Drawing.Size(311, 21);
             this.address.TabIndex = 6;
             // 
             // createButton
             // 
+            this.createButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.createButton.BackColor = System.Drawing.Color.SpringGreen;
             this.createButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.createButton.Location = new System.Drawing.Point(36, 389);
+            this.createButton.ForeColor = System.Drawing.Color.White;
+            this.createButton.Location = new System.Drawing.Point(34, 433);
             this.createButton.Name = "createButton";
             this.createButton.Size = new System.Drawing.Size(261, 39);
             this.createButton.TabIndex = 7;
@@ -130,7 +140,7 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(8, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 16);
+            this.label2.Size = new System.Drawing.Size(167, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "Nazwa użytkownika (*)";
             // 
@@ -140,7 +150,7 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.Location = new System.Drawing.Point(8, 110);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.Size = new System.Drawing.Size(77, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "Hasło (*)";
             // 
@@ -150,7 +160,7 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.Location = new System.Drawing.Point(8, 161);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 16);
+            this.label4.Size = new System.Drawing.Size(137, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "Powtórz Hasło (*)";
             // 
@@ -160,7 +170,7 @@
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label5.Location = new System.Drawing.Point(8, 211);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(117, 16);
+            this.label5.Size = new System.Drawing.Size(125, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Adres E-mail (*)";
             // 
@@ -168,9 +178,9 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(8, 270);
+            this.label6.Location = new System.Drawing.Point(7, 363);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(102, 16);
+            this.label6.Size = new System.Drawing.Size(109, 16);
             this.label6.TabIndex = 13;
             this.label6.Text = "Nr Indeksu (*)";
             // 
@@ -178,9 +188,9 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(193, 270);
+            this.label7.Location = new System.Drawing.Point(191, 311);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(113, 16);
+            this.label7.Size = new System.Drawing.Size(115, 16);
             this.label7.TabIndex = 14;
             this.label7.Text = "Data urodzenia";
             // 
@@ -188,17 +198,18 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.Location = new System.Drawing.Point(8, 325);
+            this.label8.Location = new System.Drawing.Point(7, 262);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(97, 16);
+            this.label8.Size = new System.Drawing.Size(98, 16);
             this.label8.TabIndex = 15;
             this.label8.Text = "Miejscowość";
             // 
             // index
             // 
-            this.index.Location = new System.Drawing.Point(11, 289);
+            this.index.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.index.Location = new System.Drawing.Point(10, 382);
             this.index.Name = "index";
-            this.index.Size = new System.Drawing.Size(48, 20);
+            this.index.Size = new System.Drawing.Size(60, 21);
             this.index.TabIndex = 4;
             // 
             // birthDate
@@ -206,18 +217,43 @@
             this.birthDate.AutoSize = true;
             this.birthDate.Checked = true;
             this.birthDate.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.birthDate.Location = new System.Drawing.Point(172, 272);
+            this.birthDate.Location = new System.Drawing.Point(170, 311);
             this.birthDate.Name = "birthDate";
             this.birthDate.Size = new System.Drawing.Size(15, 14);
             this.birthDate.TabIndex = 16;
             this.birthDate.UseVisualStyleBackColor = true;
             this.birthDate.CheckedChanged += new System.EventHandler(this.birthDate_CheckedChanged);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Studentem",
+            "Prowadzącym"});
+            this.comboBox1.Location = new System.Drawing.Point(10, 330);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 23);
+            this.comboBox1.TabIndex = 17;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(8, 311);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 16);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Jestem:";
+            // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.ClientSize = new System.Drawing.Size(334, 511);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.birthDate);
             this.Controls.Add(this.index);
             this.Controls.Add(this.label8);
@@ -267,5 +303,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.MaskedTextBox index;
         private System.Windows.Forms.CheckBox birthDate;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
     }
 }
--- ProjektBD/Forms/RegisterForm.cs
+++ ProjektBD/Forms/RegisterForm.cs
@@ -40,14 +40,17 @@ namespace ProjektBD.Forms
             index.Mask = "000000";
             index.ValidatingType = typeof(int);
 
+            comboBox1.SelectedIndex = 1;
+            index.Enabled = false;
+
             fillLists();
         }
-       
+
         /*
          * Uzupełnia listy z labelami i TextBoxami. Kolejność jest ważna!
          * Labele muszą być dodawane w tej samej kolejności co TextBoxy.
          * Dlaczego - patrz Button_Click.
-         */ 
+         */
         private void fillLists()
         {
             textFields.Add(login);
@@ -56,7 +59,7 @@ namespace ProjektBD.Forms
             textFields.Add(email);
             textFields.Add(index);
             textFields.Add(address);
-           
+
             // label1 - tekst informujący, label 7 - ten od daty
             labels.Add(label2);
             labels.Add(label3);
@@ -79,11 +82,26 @@ namespace ProjektBD.Forms
 
             try
             {
-                // count - 1, bo adres nieobowiązkowy
-                for (int i = 0; i < textFields.Count - 1; i++) 
+                // count - 2, bo adres nieobowiązkowy, indeks sprawdzamy osobno
+                for (int i = 0; i < textFields.Count - 2; i++)
                 {
                     if ((textFields[i].Text.Equals("")) || (textFields[i].Text.Length < 3) || (textFields[i].Text == null))
                         throw new EmptyFieldException(i);
+                }
+
+                // Indeks sprawdzamy osobno, bo tylko w niektórych przypadkach
+                if (index.Enabled)
+                {
+                    if ((index.Text.Equals("")) || (index.Text.Length < 3) || (index.Text == null))
+                        throw new EmptyFieldException(4);
+
+                    // Czy wszystkie pola w maskedTextBox indeksu są uzupełnione
+                    if (!index.MaskCompleted)
+                    {
+                        displayMsgBox("Zły nr indeksu", "Podany numer indeksu jest niepełny!");
+                        labels[4].ForeColor = Color.Red;
+                        return;
+                    }
                 }
 
                 // jeżeli hasła są różne
@@ -102,48 +120,46 @@ namespace ProjektBD.Forms
                 {
                     displayMsgBox("Zły adres e-mail", "Podany adres e-mail nie jest poprawny!");
                     labels[3].ForeColor = Color.Red;
-                    textFields[3].Text = "";
-                    return;
-                }
-
-                // Czy wszystkie pola w maskedTextBox indeksu są uzupełnione
-                if (!index.MaskCompleted)
-                {
-                    displayMsgBox("Zły nr indeksu", "Podany numer indeksu jest niepełny!");
-                    labels[4].ForeColor = Color.Red;
-                    textFields[4].Text = "";
                     return;
                 }
 
                 // Czy login/email/indeks się nie pokrywa z istniejącym użytkownikiem
-                string overlappingAttribute = isOccupied();
+                string overlappingAttribute = isOccupied(index.Enabled);
 
                 if (!overlappingAttribute.Equals(""))
                     throw new UsersOverlappingException(overlappingAttribute);
                 else
                 {
-                    MessageBox.Show("Konto zostało poprawnie założone. Możesz się zalogować.", "Koniec", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    createAccount();
+                    if (index.Enabled)
+                    {
+                        MessageBox.Show("Konto zostało poprawnie założone. Możesz się zalogować.", "Koniec", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        createAccountStudent();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Konto zostało poprawnie założone. Należy zaczekać na akceptację administratora.", "Koniec", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        notifyAdmin();
+                    }
                     this.Close();
                 }
-                
+
             }
             catch (EmptyFieldException err)
             {
                 displayMsgBox("Brak danych", "Pola muszą zawierać co najmniej 3 znaki.");
                 labels[err.getFieldNumber()].ForeColor = Color.Red;
-                
+
             }
             catch (UsersOverlappingException err)
             {
                 MessageBox.Show("Następujący " + err.getMessage() + " jest już zajęty.", "Złe dane", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            
+
         }
 
         /*
          * Funkcja do wyświetlania MsgBoxa z warningiem.
-         */ 
+         */
         private void displayMsgBox(string title, string text)
         {
             MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
@@ -216,39 +232,41 @@ namespace ProjektBD.Forms
          * bo nick i adres email trzeba sprawdzić też z prowadzącymi, indeks tylko
          * ze studentami. Metoda zwraca nazwę powtarzającego się atrybutu lub ""
          * jeżeli jest ok (nic się nie powtarza).
-         */ 
-        private string isOccupied()
-        {            
+         * bool student - jeżeli user nie jest studentem, nie sprawdzamy indeksu
+         */
+        private string isOccupied(bool student)
+        {
             String inputLogin = textFields[0].Text;
             String inputEmail = textFields[3].Text;
-            int inputIndex = Int32.Parse(index.Text);
 
             context.Użytkownicy.Load();
 
-            var query = context.Studenci.Where(s => (s.email.Equals(inputEmail)));
+            var query = context.Użytkownicy.Where(s => (s.email.Equals(inputEmail)));
 
             if (query.Count() > 0)
                 return "Email";
 
-            query = context.Studenci.Where(s => (s.login.Equals(inputLogin)));
+            query = context.Użytkownicy.Where(s => (s.login.Equals(inputLogin)));
 
             if (query.Count() > 0)
                 return "login";
 
-            context.Studenci.Load();
+            if (student)
+            {
+                context.Studenci.Load();
 
-            query = context.Studenci.Where(s => (s.nrIndeksu == inputIndex));
+                query = context.Studenci.Where(s => (s.nrIndeksu == Int32.Parse(index.Text)));
 
-            if (query.Count() > 0)
-                return "nr indeksu";
-
+                if (query.Count() > 0)
+                    return "nr indeksu";
+            }
 
             return "";
         }
 
         /*
          * Uzależniamy dateTimePicker od checkboxa
-         */ 
+         */
         private void birthDate_CheckedChanged(object sender, EventArgs e)
         {
             if (birthDate.Checked == true)
@@ -256,17 +274,17 @@ namespace ProjektBD.Forms
             else dateTimePicker1.Enabled = false;
         }
 
-        /**
+        /*
          * Metoda bazodanowa - stworzenie studenta i dodanie go do bazy.
          */
-        private void createAccount()
+        private void createAccountStudent()
         {
             Student s = new Student
             {
                 login = textFields[0].Text,
                 hasło = textFields[1].Text,
                 email = textFields[3].Text,
-                miejsceZamieszkania = textFields[5].Text,               
+                miejsceZamieszkania = textFields[5].Text,
                 nrIndeksu = Int32.Parse(index.Text)
             };
 
@@ -278,5 +296,48 @@ namespace ProjektBD.Forms
             context.SaveChanges();
         }
 
+        /*
+         * Posłanie wiadomości do admina. On akceptuje - stworzony zostaje 
+         */
+        private void notifyAdmin()
+        {
+            Prowadzący p = new Prowadzący
+            {
+                login = textFields[0].Text,
+                hasło = textFields[1].Text,
+                email = textFields[3].Text,
+                miejsceZamieszkania = textFields[5].Text
+            };
+
+            if (birthDate.Checked == true)
+                p.dataUrodzenia = dateTimePicker1.Value;
+            else p.dataUrodzenia = null;
+
+            
+            //TO DO: posłać wiadomość do admina.
+        }
+
+        /**
+         * Metoda chowająca i disable'ująca nr indeksu (na wszelki wypadek) przy wyborze "Prowadzącego"
+         */
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                index.Visible = false;
+                index.Enabled = false;
+                label6.Visible = false;
+                this.Height -= 50;
+            }
+            else
+            {
+                index.Visible = true;
+                index.Enabled = true;
+                label6.Visible = true;
+                this.Height += 50;
+            }
+        }
+
     }
+
 }
