437c7cd4c29674aceedf06f643c134349288d2d9
jakbed93 <jakbed93@gmail.com>
2015-05-17 20:03:13 +0200

    1. Stworzenie prostej formatki komunikatora. 2. Deklaracje niezbędnych metod do obsługi wiadomości.
    Niestety nie mogę otworzyć bazy, więc nie dam rady dziubnąć SQLa :(. Tam napisałem w których miejscach jest on potrzebny. Czekam na dalsze polecenia :D.

---
+++ ProjektBD/Forms/Komunikator.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ProjektBD.Forms
+{
+    partial class Komunikator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(220, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(252, 277);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 295);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(201, 302);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Wyślij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Dodaj kontakt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(110, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Usuń kontakt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Komunikator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Komunikator";
+            this.Text = "Komunikator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ ProjektBD/Forms/Komunikator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektBD.Forms
+{
+    public partial class Komunikator : Form
+    {
+        string tymczasowaWiadomosc;
+     
+        public Komunikator()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProjektBD.Model.Student nowyKontakt = new ProjektBD.Model.Student();
+            dodajKontakt(nowyKontakt);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int kontaktID = 0;
+            usunKontakt(kontaktID);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProjektBD.Model.Wiadomość wysylanaWiadomosc = new ProjektBD.Model.Wiadomość();
+
+            wysylanaWiadomosc.nadawca = "user";
+            wysylanaWiadomosc.treść = tymczasowaWiadomosc;
+            wysylanaWiadomosc.dataWysłania = DateTime.Now;
+            wysylanaWiadomosc.przeczytana = false;
+
+            wysylanieWiadomosci(wysylanaWiadomosc);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            tymczasowaWiadomosc = textBox1.Text;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Wyswietl wszystkie wiadomosci z danym rozmowca.
+            ProjektBD.Model.Wiadomość pobranaWiadomosc;
+
+            for (int i = 0; i < iloscWiadomosci; ++ i)
+            {
+                pobranaWiadomosc = pobierzWiadomosc(i);
+                Console.WriteLine(pobranaWiadomosc.dataWysłania + " - " +
+                                  pobranaWiadomosc.nadawca + " : " +
+                                  pobranaWiadomosc.treść);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ProjektBD.Model.Użytkownik pobranyKontakt;
+
+            for (int i = 0; i < iloscKontaktow; ++ i)
+            {
+                pobranyKontakt = pobierzKontakt(i);
+                Console.WriteLine(i + ". " + pobranyKontakt.login);
+            }
+        }
+
+        /**
+          *  Obsluga wiadomosci.
+          **/
+        private void wysylanieWiadomosci(ProjektBD.Model.Wiadomość wiadomoscDoWyslania)
+        {
+            // ToDo
+            // Zapisanie wiadomosci do bazy
+            Console.WriteLine("Wysylanie wiadomosci od uzytkownika do adresata: " + wiadomoscDoWyslania.treść);
+        }
+
+        private ProjektBD.Model.Wiadomość pobierzWiadomosc(int wiadomoscID)
+        {
+            // ToDo
+            // Pobranie wiadomosci z bazy
+            ProjektBD.Model.Wiadomość pobranaWiadomosc = new ProjektBD.Model.Wiadomość();
+            return pobranaWiadomosc;
+        }
+
+
+        /**
+         *  Obsluga listy kontaktow.
+         **/
+        private ProjektBD.Model.Użytkownik pobierzKontakt(int kontaktID)
+        {
+            // ToDo
+            // Pobranie kontaktu z bazy
+            ProjektBD.Model.Użytkownik pobranyKontakt = new ProjektBD.Model.Student();
+            return pobranyKontakt;
+        }
+
+        private void dodajKontakt(ProjektBD.Model.Użytkownik nowyKontakt)
+        {
+            // ToDo
+            // Dodanie kontaktu do bazy
+        }
+
+        private void usunKontakt(int kontaktID)
+        {
+            // ToDo
+            // Usuniecie kontaktu z bazy
+        }
+    }
+}
